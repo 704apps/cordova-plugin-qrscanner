82fe2b0aca65e7946b5a3e945f9c3d6d3bd7b1f7
Cristian Barbosa <an.cristianbarbosa.an@gmail.com>
2025-02-05 16:06:39 -0300

    ajustando build

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 'use strict';
 
-const gulp = require('gulp');
+const gulp   = require('gulp');
 const insert = require('gulp-insert');
-const fs= require('fs');
+const fs     = require('fs');
+const exec   = require('child_process').exec;
 
+// Lê o conteúdo do arquivo de remapeamento
 const remap = fs.readFileSync('src/common/src/cordova-remap.js', 'utf-8');
 
-function webpack(config, callback){
-  const exec = require('child_process').exec;
+// Função auxiliar para chamar o webpack com uma determinada configuração
+function webpackTask(config, callback) {
   exec(__dirname + '/node_modules/.bin/webpack --config ' + config, (error, stdout, stderr) => {
     console.log(stdout);
     console.log(stderr);
@@ -15,32 +17,56 @@ function webpack(config, callback){
   });
 }
 
-gulp.task('prepack', function(cb){
-  webpack('webpack.prepack.config.js', cb);
-});
+// Tarefa "prepack"
+function prepack(cb) {
+  webpackTask('webpack.prepack.config.js', cb);
+}
 
-gulp.task('webpack-cordova', ['prepack'], function(cb){
-  webpack('webpack.cordova.config.js', cb);
-});
+// Tarefa "webpack-cordova" que depende de "prepack"
+function webpackCordova(cb) {
+  webpackTask('webpack.cordova.config.js', cb);
+}
+const webpackCordovaSeries = gulp.series(prepack, webpackCordova);
 
-gulp.task('dist', ['prepack'], function(cb){
-  webpack('webpack.library.config.js', cb);
-});
+// Tarefa "dist" que depende de "prepack"
+function dist(cb) {
+  webpackTask('webpack.library.config.js', cb);
+}
+const distSeries = gulp.series(prepack, dist);
 
-gulp.task('remap', ['webpack-cordova'], function () {
+// Tarefa "remap" que depende de "webpack-cordova"
+function remapTask() {
   return gulp.src(['dist/plugin.min.js', 'dist/www.min.js'])
-  .pipe(insert.prepend(remap))
-  .pipe(gulp.dest('dist'));
-});
+    .pipe(insert.prepend(remap))
+    .pipe(gulp.dest('dist'));
+}
+const remapSeries = gulp.series(webpackCordovaSeries, remapTask);
 
-gulp.task('plugin', ['remap'], function () {
+// Tarefa "plugin" que depende de "remap"
+function pluginTask() {
   return gulp.src(['dist/plugin.min.js'])
-  .pipe(gulp.dest('src/browser'));
-});
+    .pipe(gulp.dest('src/browser'));
+}
+const pluginSeries = gulp.series(remapSeries, pluginTask);
 
-gulp.task('www', ['remap'], function () {
+// Tarefa "www" que depende de "remap"
+function wwwTask() {
   return gulp.src(['dist/www.min.js'])
-  .pipe(gulp.dest('www'));
-});
+    .pipe(gulp.dest('www'));
+}
+const wwwSeries = gulp.series(remapSeries, wwwTask);
 
-gulp.task('default', ['dist', 'plugin', 'www']);
+// Tarefa "default": executa em paralelo "dist", "plugin" e "www"
+const defaultTask = gulp.parallel(distSeries, pluginSeries, wwwSeries);
+
+// Registra as tarefas no gulp
+gulp.task('prepack', prepack);
+gulp.task('webpack-cordova', webpackCordovaSeries);
+gulp.task('dist', distSeries);
+gulp.task('remap', remapSeries);
+gulp.task('plugin', pluginSeries);
+gulp.task('www', wwwSeries);
+gulp.task('default', defaultTask);
+
+// Exporta a tarefa default
+exports.default = defaultTask;
